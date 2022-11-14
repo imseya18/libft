318bff76a599d5008654dc1f8e35217747181591
Mathieu Morue <mmorue@k0r2p8.42mulhouse.fr>
2022-11-14 15:43:16 +0100

    push final manque un bonus

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: mmorue <mmorue@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/31 15:35:55 by mmorue            #+#    #+#             */
-/*   Updated: 2022/11/11 13:56:52 by mmorue           ###   ########.fr       */
+/*   Updated: 2022/11/14 15:40:07 by mmorue           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -58,4 +58,10 @@ char			*ft_strtrim(char const *s1, char const *set);
 t_list			*ft_lstnew(void *content);
 void			ft_lstadd_front(t_list **lst, t_list *new);
 int				ft_lstsize(t_list *lst);
+t_list			*ft_lstlast(t_list *lst);
+void			ft_lstadd_back(t_list **lst, t_list *new);
+void			ft_lstdelone(t_list *lst, void (*del)(void *));
+void			ft_lstclear(t_list **lst, void (*del)(void *));
+void			ft_lstiter(t_list *lst, void (*f)(void *));
+t_list 			*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *));
 #endif
--- makefile
+++ makefile
@@ -37,7 +37,13 @@ SOURCES =   ft_atoi.c \
 
 SOURCE_BONUS = 	ft_lstnew.c \
 				ft_lstsize.c \
-				ft_lstadd_front.c 
+				ft_lstadd_front.c \
+				ft_lstlast.c \
+				ft_lstadd_back.c \
+				ft_lstdelone.c \
+				ft_lstclear.c \
+				ft_lstiter.c \
+				ft_lstmap.c
 
 OBJECTS = $(SOURCES:.c=.o)
 
